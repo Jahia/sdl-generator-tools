890a8141c85989fed313e18c77a3128063e6b3a8
akarmanov <alexandrk_@hotmail.com>
2019-02-13 14:09:34 -0500

    BACKLOG-9632 Add dialog

--- src/main/javascript/app/stepperComponents/createTypes/CreateTypes.jsx
+++ src/main/javascript/app/stepperComponents/createTypes/CreateTypes.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import {Grid, Paper, List, ListItem, ListItemText, ListSubheader, Button} from '@material-ui/core';
 import {Add} from '@material-ui/icons';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+const AddTypeDialog = ({open, closeDialog}) => (
+    <Dialog
+        open={open}
+        aria-labelledby="form-dialog-title"
+        onClose={closeDialog}
+    >
+        <DialogTitle id="form-dialog-title">Add new type</DialogTitle>
+        <DialogContent>
+            <TextField
+                autoFocus
+                fullWidth
+                margin="dense"
+                id="typeName"
+                label="Custom type name"
+                type="text"
+            />
+        </DialogContent>
+        <DialogActions>
+            <Button color="primary" onClick={closeDialog}>
+                Cancel
+            </Button>
+            <Button color="primary" onClick={closeDialog}>
+                Add
+            </Button>
+        </DialogActions>
+    </Dialog>
+);
 
 const TypeItem = ({name}) => (
     <ListItem>
@@ -9,33 +42,40 @@ const TypeItem = ({name}) => (
     </ListItem>
 );
 
-const CreateTypes = ({nodeTypes, addType}) => (
-    <Grid container>
-        <Grid item>
-            <Paper>
-                <List subheader={<ListSubheader>Node type</ListSubheader>}>
-                    <Button onClick={() => addType({typeName: 'SuperType', nodeType: 'jnt;something'})}>
-                        Add new type
-                        <Add/>
-                    </Button>
-                    {
-                        nodeTypes.map(type => (<TypeItem key={type.name} {...type}/>))
-                     }
-                </List>
-            </Paper>
-        </Grid>
-        <Grid item>
-            <Paper>
-                <List subheader={<ListSubheader>Properties</ListSubheader>}>
-                    <Button>
-                        Add a new property
-                        <Add/>
-                    </Button>
-                </List>
-            </Paper>
-        </Grid>
-    </Grid>
-);
+const CreateTypes = ({nodeTypes, addType}) => {
+    const [addTypeDialogShown, showAddTypeDialog] = useState(false);
+
+    return (
+        <React.Fragment>
+            <Grid container>
+                <Grid item>
+                    <Paper>
+                        <List subheader={<ListSubheader>Node type</ListSubheader>}>
+                            <Button onClick={() => showAddTypeDialog(true)}>
+                                Add new type
+                                <Add/>
+                            </Button>
+                            {
+                                nodeTypes.map(type => (<TypeItem key={type.name} {...type}/>))
+                            }
+                        </List>
+                    </Paper>
+                </Grid>
+                <Grid item>
+                    <Paper>
+                        <List subheader={<ListSubheader>Properties</ListSubheader>}>
+                            <Button>
+                                Add a new property
+                                <Add/>
+                            </Button>
+                        </List>
+                    </Paper>
+                </Grid>
+            </Grid>
+            <AddTypeDialog open={addTypeDialogShown} closeDialog={() => showAddTypeDialog(false)}/>
+        </React.Fragment>
+    );
+};
 
 CreateTypes.propTypes = {
     nodeTypes: PropTypes.array.isRequired,
