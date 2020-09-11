15e0711ca8fac94f900822c7589c3d373a513f3a
Thomas Draier <tdraier@jahia.com>
2020-09-11 11:53:46 +0200

    BACKLOG-14534: Use dedicated jsonp function

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,8 @@ module.exports = (env, argv) => {
         },
         output: {
             path: path.resolve(__dirname, 'src/main/resources/javascript/apps/'),
-            filename: 'jahia.bundle.js'
+            filename: 'jahia.bundle.js',
+            jsonpFunction: 'sdlGeneratorToolsJsonp'
         },
         resolve: {
             mainFields: ['module', 'main'],
